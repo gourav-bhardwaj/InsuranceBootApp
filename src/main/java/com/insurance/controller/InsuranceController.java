f583983cee1f1f7334dbe75e2b9a4fa064a6cf2a
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-10-24 02:09:08 +0530

    Added Insurance Controller and POM modified.

---
+++ src/main/java/com/insurance/controller/InsuranceController.java
@@ -0,0 +1,25 @@
+package com.insurance.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/insurance")
+public class InsuranceController {
+
+    @GetMapping("/demo")
+    public ResponseEntity<Map<String, Object>> demo() {
+        Map<String, Object> respMap = new HashMap<>();
+        respMap.put("message", "This is a demo message!");
+        respMap.put("timestamp", LocalDateTime.now());
+        return new ResponseEntity<>(respMap, HttpStatus.OK);
+    }
+
+}
