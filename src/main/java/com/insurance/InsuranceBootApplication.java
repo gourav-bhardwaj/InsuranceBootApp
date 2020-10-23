156d2612ec8711ccc58746ae79f978383d0a4208
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-10-24 01:46:50 +0530

    Initial commit

---
+++ src/main/java/com/insurance/InsuranceBootApplication.java
@@ -0,0 +1,13 @@
+package com.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceBootApplication.class, args);
+	}
+
+}
