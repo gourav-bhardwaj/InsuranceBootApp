e4449a134add507904426ae8c09d0327ede3e0d3
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-10-31 21:48:22 +0530

    Updated InsuranceController.java.

--- src/main/java/com/insurance/controller/InsuranceController.java
+++ src/main/java/com/insurance/controller/InsuranceController.java
@@ -22,7 +22,7 @@ public class InsuranceController {
         return new ResponseEntity<>(respMap, HttpStatus.OK);
     }
 
-    @GetMapping("/hi")
+    @GetMapping("/joker")
     public ResponseEntity<Map<String, Object>> app() {
         Map<String, Object> respMap = new HashMap<>();
         respMap.put("message", "Hi, i m joker!");
