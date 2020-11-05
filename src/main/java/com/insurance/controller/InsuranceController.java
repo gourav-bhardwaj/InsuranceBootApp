badf0f1a699f5a07e5ac1e2b2a2c3dd5f389364f
Gourav <gov.kumarbharatdwaj@gmail.com>
2020-11-05 20:49:09 +0530

    Commit InsuranceController.java

--- src/main/java/com/insurance/controller/InsuranceController.java
+++ src/main/java/com/insurance/controller/InsuranceController.java
@@ -22,7 +22,7 @@ public class InsuranceController {
         return new ResponseEntity<>(respMap, HttpStatus.OK);
     }
 
-    @GetMapping("/demo")
+    @GetMapping("/kapil")
     public ResponseEntity<Map<String, Object>> app() {
         Map<String, Object> respMap = new HashMap<>();
         respMap.put("message", "Hi, i m joker!");
